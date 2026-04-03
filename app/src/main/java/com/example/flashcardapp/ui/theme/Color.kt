817529ca179bedad3182a873c68eb933e84dbf6f
vuxuantuan555-cmyk <email_cua_ban@gmail.com>
2026-04-03 18:02:32 +0700

    style: update theme colors for flashcard UI

--- app/src/main/java/com/example/flashcardapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/flashcardapp/ui/theme/Color.kt
@@ -6,6 +6,16 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
+val Purple40 = Color(0xFF6650A4)
+val PurpleGrey40 = Color(0xFF625B71)
 val Pink40 = Color(0xFF7D5260)
+
+// Review button colors
+val ColorAgain = Color(0xFFE53935)   // Đỏ - Again
+val ColorHard  = Color(0xFFFF7043)   // Cam - Hard
+val ColorGood  = Color(0xFF43A047)   // Xanh lá - Good
+val ColorEasy  = Color(0xFF1E88E5)   // Xanh dương - Easy
+
+// Card colors
+val CardFront  = Color(0xFF1E1E2E)
+val CardBack   = Color(0xFF2A2A3E)
