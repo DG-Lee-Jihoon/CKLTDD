1d7fb582e35b9945365afdb3a5e776111e174642
khaivo05 <khaican6@gmail.com>
2026-04-03 19:31:14 +0700

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,23 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.duancuoiki"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.duancuoiki"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core-ktx:1.9.0")
 }
