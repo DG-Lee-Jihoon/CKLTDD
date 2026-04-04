7716be86700e7468b57f106ac09eb7037fa1684e
vuxuantuan555-cmyk <email_cua_ban@gmail.com>
2026-04-04 21:03:10 +0700

    feat: integrate Firebase Cloud and Internet permissions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("com.google.devtools.ksp")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -42,7 +43,12 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
+// Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
 
+// Thêm thư viện Firestore (để đồng bộ dữ liệu) và Auth (để đăng nhập)
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-auth")
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
