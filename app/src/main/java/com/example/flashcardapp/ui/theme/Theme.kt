29271ee52dfc64c2c918f59f9f4c625700dbeaf8
vuxuantuan555-cmyk <email_cua_ban@gmail.com>
2026-04-04 19:45:55 +0700

    feat: update app theme colors

--- app/src/main/java/com/example/flashcardapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/flashcardapp/ui/theme/Theme.kt
@@ -12,25 +12,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = AccentPurple,
+    secondary = AccentViolet,
+    tertiary = ColorEasy,
+    background = BgDeep,
+    surface = BgCard
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = AccentPurple,
+    secondary = AccentViolet,
+    tertiary = ColorEasy,
+    background = BgDeep,
+    surface = BgCard
 )
 
 @Composable
@@ -52,7 +46,7 @@ fun FlashcardAppTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+
         content = content
     )
 }
